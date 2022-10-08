458193c5f1d905a69f038314125ba6140e999e34
haquyetchien-ungdungphanmem <haquyetchien.ungdungphanmem@gmail.com>
2022-10-08 21:22:38 +0700

    Revert "chienhq_commit_category"
    This reverts commit 0ddb126fde0d518e9a2b1dbaf5e1aa6e8d1fe71a.

--- src/main/java/com/fix/mobile/repository/CategoryRepository.java
+++ src/main/java/com/fix/mobile/repository/CategoryRepository.java
@@ -2,8 +2,6 @@ package com.fix.mobile.repository;
 
 import com.fix.mobile.entity.Category;
 import com.fix.mobile.entity.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +11,4 @@ import java.util.List;
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
     //anhhv
     List<Category> findByType(Boolean type);
-
-    Page<Category> findByType(boolean type,Pageable pageable);
-
-    Page<Category> findByName(String name, Pageable pageable);
 }
--- src/main/java/com/fix/mobile/service/CategoryService.java
+++ src/main/java/com/fix/mobile/service/CategoryService.java
@@ -1,8 +1,6 @@
 package com.fix.mobile.service;
 
 import com.fix.mobile.entity.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
