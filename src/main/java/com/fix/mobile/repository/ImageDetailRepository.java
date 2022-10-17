b6f14b922665c51cf074a170a24b7b3a2c229c3a
anhhv492 <anhhv492@gmail.com>
2022-10-17 19:28:26 +0700

    Delete ImageDetailRepository.java

--- src/main/java/com/fix/mobile/repository/ImageDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fix.mobile.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageDetailRepository extends PagingAndSortingRepository<ImageDetail, Integer> {
-}
