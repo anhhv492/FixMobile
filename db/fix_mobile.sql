2f6fd9d555b89f50c331061477fbc47e26f1e804
anhhv492 <105498193+anhhv492@users.noreply.github.com>
2022-11-15 15:16:02 +0700

    Update fix_mobile.sql

--- db/fix_mobile.sql
+++ db/fix_mobile.sql
@@ -445,15 +445,19 @@ INSERT INTO fix_mobile.products (name,create_date,camera,price,size,note,status,
 id_ram,id_color,id_capacity,id_category,id_image) 
  VALUES ('IPhone XS Max', '2022-10-06', '1280px, cảm biến LiDAR', 20000000, '20x25cm', 'Bền, đẹp mắt', 1,1,1,1,13,1);
 select*from orders;
+
 INSERT INTO fix_mobile.accounts(username,password,full_name,gender,email,
-								phone,create_date,image,id_role) 
-VALUES ('admin','123','admin',0,'admin@gmail.com','0912321321','2022/10/30','',1);
+				phone,create_date,image,id_role) 
+VALUES ('admin','$2a$12$nSbqR9l9BenII9tUcUoHXeDb77X.x59UZPMB894ynZ8z8I4WDSLOO','admin',0,'admin@gmail.com',
+	'0912321321','2022/10/30','',1);
 INSERT INTO fix_mobile.accounts(username,password,full_name,gender,email,
-								phone,create_date,image,id_role) 
-VALUES ('vietanhvs','492002','Hạ Việt Anh',0,'vietanhvs@gmail.com','0984297473','2022/10/30','',1);
+				phone,create_date,image,id_role) 
+VALUES ('user','$2a$12$SYSb7NU1cND3wct0LRWrxe3id70DF6PeGbhkbcFATooYDKe4wyyqe','user',1,'user@gmail.com',
+	'0123456789','2002/11/11','',3);
 INSERT INTO fix_mobile.accounts(username,password,full_name,gender,email,
-								phone,create_date,image,id_role) 
-VALUES ('user','123','user',1,'user@gmail.com','0123456789','2002/11/11','',3);
+				phone,create_date,image,id_role) 
+VALUES ('vietanhvs','$2a$12$FUNIidYXB/rc3BRR1XuQZObS4Vn7BPPomqllVvwcBOkJtZJWKFM16','Hạ Việt Anh',0,'vietanhvs@gmail.com',
+	'0984297473','2022/10/30','',1);
 
 select*from accounts;
 select*from roles;
