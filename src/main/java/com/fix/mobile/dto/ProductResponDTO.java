8441ef12fb25d9728954fd9ac2d1a0241e40dd62
haquyetchien-ungdungphanmem <haquyetchien.ungdungphanmem@gmail.com>
2022-12-14 01:27:57 +0700

    commit home

--- src/main/java/com/fix/mobile/dto/ProductResponDTO.java
+++ src/main/java/com/fix/mobile/dto/ProductResponDTO.java
@@ -1,2 +1,14 @@
-package com.fix.mobile.dto;public class ProductResponDTO {
+package com.fix.mobile.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductResponDTO {
+    private Integer idProduct;
+    private String name;
+    private String image;
+    private BigDecimal price;
+    private Integer totail;
 }
