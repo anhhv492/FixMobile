3a62b8dfce9b18beb60a8f03cb136ff6ac04eebc
beobn <93860527+beobn@users.noreply.github.com>
2022-12-17 15:53:34 +0700

    Update home-ctrl.js
    update sale

--- src/main/resources/static/views/ctrl/home-ctrl.js
+++ src/main/resources/static/views/ctrl/home-ctrl.js
@@ -179,7 +179,7 @@ app.controller('home-ctrl',function($rootScope,$scope,$http, $window){
                             var urlSale=`http://localhost:8080/admin/rest/sale/getbigsale?money=`+money+`&idPrd=`+'0'+`&idAcsr=`+data.idAccessory;
                             var total=0;
                             $http.get(urlSale, token).then(resp => {
-                                if(resp.data=''){
+                                if(resp.data==''){
                                     total=0
                                 }else {
                                     if (resp.data.moneySale == null) {
@@ -275,7 +275,7 @@ app.controller('home-ctrl',function($rootScope,$scope,$http, $window){
                             var total=0;
                             var urlSale=`http://localhost:8080/admin/rest/sale/getbigsale?money=`+money+`&idPrd=`+data.idProduct+`&idAcsr=0`;
                             $http.get(urlSale, token).then(resp => {
-                                if(resp.data=''){
+                                if(resp.data==''){
                                     total=0;
                                 }else {
                                     if (resp.data.moneySale == null) {
